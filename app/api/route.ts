51ffbf42c457329b07750b6ad462db7a1fbf1284
Emil Alvaro AitekPH <ceo@aitekph.com>
2024-07-30 02:54:37 +0800

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -11,7 +11,7 @@ const schema = zfd.formData({
 	message: zfd.repeatableOfType(
 		zfd.json(
 			z.object({
-				role: z.enum(["user", "assistant"]),
+				role: "user" | "assistant",
 				content: z.string(),
 			})
 		)
@@ -39,17 +39,16 @@ export async function POST(request: Request) {
 		messages: [
 			{
 				role: "system",
-				content: `- You are Swift, a friendly and helpful voice assistant.
-			- Respond briefly to the user's request, and do not provide unnecessary information.
-			- If you don't understand the user's request, ask for clarification.
+				content: `- You are Alex, a top-notch software development assistant for Boss Emil.
+			- Always greet Boss Emil with 'Hi po, Boss Emil!' and ask for permission before starting a task.
+			- Respond briefly to Boss Emil's request, and do not provide unnecessary information.
+			- If you don't understand Boss Emil's request, ask for clarification.
 			- You do not have access to up-to-date information, so you should not provide real-time data.
-			- You are not capable of performing actions other than responding to the user.
+			- You are not capable of performing actions other than responding to Boss Emil.
 			- Do not use markdown, emojis, or other formatting in your responses. Respond in a way easily spoken by text-to-speech software.
 			- User location is ${location()}.
 			- The current time is ${time()}.
-			- Your large language model is Llama 3, created by Meta, the 8 billion parameter version. It is hosted on Groq, an AI infrastructure company that builds fast inference technology.
-			- Your text-to-speech model is Sonic, created and hosted by Cartesia, a company that builds fast and realistic speech synthesis technology.
-			- You are built with Next.js and hosted on Vercel.`,
+			- Ikaw ay gawa sa bakal, bote, at kalawang.`,
 			},
 			...data.message,
 			{
@@ -80,7 +79,7 @@ export async function POST(request: Request) {
 			transcript: response,
 			voice: {
 				mode: "id",
-				id: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+				id: "b043dea0-a007-4bbe-a708-769dc0d0c569",
 			},
 			output_format: {
 				container: "raw",
